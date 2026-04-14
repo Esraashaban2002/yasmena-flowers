04d50b9fba1b10b31eb09ca63fed63a176b11290
Esraa Shaban <esraashaban2231@gmail.com>
2026-04-14 16:25:46 +0200

    hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CartProvider } from './context/CartContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <BrowserRouter> 
+   <HashRouter> 
     <CartProvider >
       <App />
     </CartProvider>
-   </BrowserRouter>
+   </HashRouter>
   </StrictMode>,
 )
