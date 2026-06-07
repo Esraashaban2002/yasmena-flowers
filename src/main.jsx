1618443b035d5c0158f6ff55364c88c195889b49
Esraa Shaban <esraashaban2231@gmail.com>
2026-06-07 17:52:39 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/yasmena-flowers">
+    <BrowserRouter>
       <LanguageProvider>
         <CartProvider>
           <App />
